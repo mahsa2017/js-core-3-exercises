f83b85479687c37673c695f4f58b8324e6766031
mahsa2017 <upmahjour@gmail.com>
2018-02-19 22:12:00 +0000

    findNeedle jest added more tests

--- jest/findNeedle.test.js
+++ jest/findNeedle.test.js
@@ -3,6 +3,14 @@ function findNeedle(haystack) {
     return haystack.indexOf('needle');
   }
   var haystack1 = ['pony', 'water', 'keyboard', 'needle', 'cactus'];
+  var haystack2 = ['glass', 'needle']
+  var haystack3 = ['cat','dogs','needles']
   test('index of needle in this haystack1 is',function(){
       expect(findNeedle(haystack1)).toBe(3);
   });
+  test('index of needle in this haystack2 is',function(){
+    expect(findNeedle(haystack2)).toBe(1);
+});
+test('index of needle in this haystack3 is',function(){
+    expect(findNeedle(haystack3)).toBe(-1);
+});
